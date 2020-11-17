ee978480c916db11f2a8fa930aad67859ec44a90
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 14:45:52 +0600

    Update and rename JavaScript to JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,7 +1,5 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
-const pi=3.14159;
+const pi = 3.14159;
 var A = parseFloat(lines.shift());
-
-
-console.log("A=" + (pi*A*A).toFixed(4));
+console.log("A=" + (pi * A * A).toFixed(4));
